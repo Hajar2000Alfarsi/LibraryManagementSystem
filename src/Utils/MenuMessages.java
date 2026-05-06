9982a934881f7c26a0aa09b27d78d0d2b0b510a1
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 09:00:45 +0400

    messages for menu

---
+++ src/Utils/MenuMessages.java
@@ -0,0 +1,26 @@
+package Utils;
+
+public class MenuMessages {
+    public static String MAIN_MENU_MASSAGE =
+            "1- Library Menu" +
+            "2- Items Menu" +
+            "3- Members Menu" +
+             "4- Exit";
+    public static String LIBRARY_MENU_MASSAGE =
+            "1- Show Library name" +
+            "2- About Library" +
+            "3- Address of Library" +
+            "4- Exit" ;
+    public static String MEMEBERS_MENU_MASSAGE =
+            "1- Register new member" +
+            "2- Modify member name" +
+            "3- Display member" +
+            "4- Exit" ;
+    public static String ITEMS_MENU_MASSAGE =
+            "1- Add new Item" +
+            "2- Search Item" +
+            "3- Borrow Item" +
+            "4- Return Item" +
+            "5- View borrowed" +
+            "6- Exit" ;
+}
