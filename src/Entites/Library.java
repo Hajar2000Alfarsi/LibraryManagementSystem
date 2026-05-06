0381e41cb00d553e26d4fc938fef66ccdfd25dd2
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 08:44:15 +0400

    Assign objects to library class

--- src/Entites/Library.java
+++ src/Entites/Library.java
@@ -1,5 +1,22 @@
 package Entites;
 
 public class Library {
+    private String libraryName;
+    private String aboutLibrary;
 
+    public String getLibraryName() {
+        return libraryName;
+    }
+
+    public void setLibraryName(String libraryName) {
+        this.libraryName = libraryName;
+    }
+
+    public String getAboutLibrary() {
+        return aboutLibrary;
+    }
+
+    public void setAboutLibrary(String aboutLibrary) {
+        this.aboutLibrary = aboutLibrary;
+    }
 }
