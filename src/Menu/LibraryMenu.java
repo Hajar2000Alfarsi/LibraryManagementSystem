6da3b21724fb83a83693f64dd71649418eea2820
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 09:04:50 +0400

    Implementation for menu

---
+++ src/Menu/LibraryMenu.java
@@ -0,0 +1,13 @@
+package Menu;
+
+public class LibraryMenu implements MenuInterface{
+    @Override
+    public void displayMenu() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
+}
---
+++ src/Menu/Menu.java
@@ -0,0 +1,14 @@
+package Menu;
+
+public class Menu implements MenuInterface{
+
+    @Override
+    public void displayMenu() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
+}
