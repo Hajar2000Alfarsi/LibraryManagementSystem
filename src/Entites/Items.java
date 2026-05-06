a27ecbca3ca90c09cd271c77124d4699f3c370a9
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 08:39:23 +0400

    Assign objects to Items class

--- src/Entites/Items.java
+++ src/Entites/Items.java
@@ -1,4 +1,22 @@
 package Entites;
 
 public class Items {
+    private String author;
+    private Boolean status;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
