822ecf5fba2fe0aeb64c452df39195ad59976021
Hajar <hajar2000alfarsi@gmail.com>
2026-05-06 08:40:47 +0400

    Assign objects to members class

--- src/Entites/Members.java
+++ src/Entites/Members.java
@@ -1,4 +1,22 @@
 package Entites;
 
 public class Members {
+    private String phoneNumber;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
